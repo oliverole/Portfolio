43d231d60027d308230c31960623e5afea836928
Oliver <olgo0001@stud.kea.dk>
2024-02-05 08:09:29 +0100

    Færdigt site

---
+++ burgermenu/button/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
+
+    menuToggle.addEventListener("click", function () {
+        const expanded = menuToggle.getAttribute("aria-expanded") === "true" || false;
+        menuToggle.setAttribute("aria-expanded", !expanded);
+        menu.classList.toggle("show");
+    });
+});
